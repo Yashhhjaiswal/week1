3166b359e2702f673e4999348ae1146e6904a45b
yash <yj4168@srmist.edu.in>
2026-03-12 10:45:15 +0530

    uc5

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,24 @@
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args){
-
         // Original string
-        String original = "racecar";
-
-        char[] chars = original.toCharArray();
-        int left = 0;
-        int right = chars.length - 1;
-
-        boolean isPalindrome = true;
-
-        // Two-pointer comparison
-        while (left < right) {
-            if (chars[left] != chars[right]) {
-                isPalindrome = false;
-                break;
-            }
-            left++;
-            right--;
+        String original = "level";
+        Stack<Character> stack = new Stack<>();
+        // Push each character into the stack
+        for (int i = 0; i < original.length(); i++) {
+            stack.push(original.charAt(i));
         }
-        if (isPalindrome) {
+        // Build reversed string by popping characters
+        String reversed = "";
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
+        }
+        // Compare original and reversed
+        if (original.equals(reversed)) {
             System.out.println(original + " is a Palindrome.");
         } else {
             System.out.println(original + " is NOT a Palindrome.");
         }
-
-
     }
 }
